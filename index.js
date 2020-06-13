de2bc830869c6b979a6474da7e50609c150d32ce
nw <nico.walters@techtalents.es>
2020-06-13 13:27:51 +0200

    searchArtist progress

--- index.js
+++ index.js
@@ -23,12 +23,26 @@ function authenticate() {
         }
     }).then(response => {
         token.access = response.data.access_token;
-        token.timeLimit = Date.now() + response.data.expires_in * 1000
-        console.log(token)
-    }).catch(error => {
-        console.log(error.response.data);
+        token.timeLimit = Date.now() + response.data.expires_in * 1000;
+        console.log(token);
     })
+    .catch(error => {
+        console.log(error.response.data);
+    });
 }
 
-authenticate();
-console.log(token)
+function searchArtist(artist) {
+    const endpoint = "https://api.spotify.com/v1/search";
+
+    axios({
+        method: "get",
+        url: endpoint,
+        headers: {
+            Authorization: "Bearer " + token.access
+        },
+        params: {
+            q: artist,
+            type: "artist"
+        }
+    })
+}
