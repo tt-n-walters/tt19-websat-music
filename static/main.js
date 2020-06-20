69507678a691b90897e0d123939e3cb854c0c017
nw <nico.walters@techtalents.es>
2020-06-20 13:17:14 +0200

    First js

---
+++ static/main.js
@@ -0,0 +1,12 @@
+let input, searches, tracks;
+
+
+function setup() {
+    input = document.getElementById("input");
+    searches = document.getElementById("searches");
+    tracks = document.getElementById("tracks");
+}
+
+
+window.addEventListener("load", setup);
+
