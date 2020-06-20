d9fb2e91a75cd555d55181d10bcee98319c1282b
nw <nico.walters@techtalents.es>
2020-06-20 13:19:40 +0200

    More js

--- static/main.js
+++ static/main.js
@@ -5,6 +5,16 @@ function setup() {
     input = document.getElementById("input");
     searches = document.getElementById("searches");
     tracks = document.getElementById("tracks");
+
+    input.addEventListener("keypress", handleKeypress);
+}
+
+function handleKeypress(event) {
+    if (event.code == "Enter") {
+        let artist = input.value;
+        let url = "/artist?name=" + artist
+        
+    }
 }
 
 
